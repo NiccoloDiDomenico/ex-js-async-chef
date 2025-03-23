89167cca06cf03bc311e0ee239f8e2a79286352f
NiccoloDiDomenico <niccolodidomenico99@gmail.com>
2025-03-23 14:08:16 +0100

    Fix the code

--- main.js
+++ main.js
@@ -3,9 +3,9 @@
 //     try {
 //         const response = await fetch(`https://dummyjson.com/recipes/${id}`)
 //         const recipe = await response.json()
-//         const secondResponse = await fetch(`https://dummyjson.com/users/${recipe.userId}`)
-//         const user = await secondResponse.json()
-//         return user.birthDate
+//         const secondResponse = await fetch(`https://dummyjson123.com/users/${recipe.userId}`)
+//         const chef = await secondResponse.json()
+//         return chef.birthDate
 //     } catch (error) {
 //         throw new Error(`Errore interno alla funzione: ${error.message}`)
 //     }
@@ -15,6 +15,15 @@
 //     .then(birthdate => console.log(`Data di nascita dello chef: ${birthdate}`))
 //     .catch(error => console.error(error))
 
+// (async () => {
+//     try {
+//         const result = await getChefBirthday(1)
+//         console.log(`Data di nascita dello chef: ${result}`);
+//     } catch (error) {
+//         console.error(error)
+//     }
+// })();
+
 
 // Bonus 1
 // async function getChefBirthday(id) {
@@ -23,27 +32,25 @@
 //     try {
 //         const response = await fetch(`https://dummyjson.com/recipes/${id}`)
 //         recipe = await response.json()
+//         if (recipe.message) {
+//             throw new Error(recipe.message)
+//         }
 //     } catch (error) {
 //         throw new Error(`Errore durante il fetch della ricetta ${id}: ${error.message}`)
 //     }
 
-//     if (recipe.message) {
-//         throw new Error(recipe.message)
-//     }
-
-//     let user
+//     let chef
 //     try {
 //         const secondResponse = await fetch(`https://dummyjson.com/users/${recipe.userId}`)
-//         user = await secondResponse.json()
+//         chef = await secondResponse.json()
+//         if (chef.message) {
+//             throw new Error(chef.message)
+//         }
 //     } catch (error) {
-//         throw new Error(`Errore durante il fetch dell'user ${recipe.userId}: ${error.message}`)
+//         throw new Error(`Errore durante il fetch dello chef ${recipe.userId}: ${error.message}`)
 //     }
 
-//     if (user.message) {
-//         throw new Error(user.message)
-//     }
-
-//     return user.birthDate
+//     return chef.birthDate
 // }
 
 // getChefBirthday(1)
@@ -51,41 +58,54 @@
 //     .catch(error => console.error(error))
 //     .finally(() => console.log(`Fine dell'operazione`))
 
+// (async () => {
+//     try {
+//         const result = await getChefBirthday(1)
+//         console.log(`Data di nascita dello chef: ${result}`);
+//     } catch (error) {
+//         console.error(error)
+//     }
+// })();
 
-// Importa dayjs
-const dayjs = require('dayjs');
 
-// Bonus 2
+// // Bonus 2
 async function getChefBirthday(id) {
 
     let recipe;
     try {
         const response = await fetch(`https://dummyjson.com/recipes/${id}`)
         recipe = await response.json()
+        if (recipe.message) {
+            throw new Error(recipe.message)
+        }
     } catch (error) {
         throw new Error(`Errore durante il fetch della ricetta ${id}: ${error.message}`)
-    }
-
-    if (recipe.message) {
-        throw new Error(recipe.message)
     }
 
     let user
     try {
         const secondResponse = await fetch(`https://dummyjson.com/users/${recipe.userId}`)
         user = await secondResponse.json()
+        if (user.message) {
+            throw new Error(user.message)
+        }
     } catch (error) {
         throw new Error(`Errore durante il fetch dell'user ${recipe.userId}: ${error.message}`)
-    }
-
-    if (user.message) {
-        throw new Error(user.message)
     }
 
     return dayjs(user.birthDate).format('DD/MM/YYYY')
 }
 
-getChefBirthday(1)
-    .then(birthdate => console.log(`Data di nascita dello chef: ${birthdate}`))
-    .catch(error => console.error(error))
-    .finally(() => console.log(`Fine dell'operazione`))
+// getChefBirthday(1)
+//     .then(birthdate => console.log(`Data di nascita dello chef: ${birthdate}`))
+//     .catch(error => console.error(error))
+//     .finally(() => console.log(`Fine dell'operazione`))
+
+(async () => {
+    try {
+        const result = await getChefBirthday(1)
+        console.log(`Data di nascita dello chef: ${result}`);
+    } catch (error) {
+        console.error(error)
+    }
+})();
